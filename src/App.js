f39389c3b590bccc5be004198adce772f0e47bc0
Yuvraj Soni <demonsroar2172@gmail.com>
2023-09-22 01:17:07 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,68 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-  return <>hii</>;
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Charger", quantity: 10, packed: true },
+];
+
+const App = () => {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Status />
+    </div>
+  );
+};
+
+function Logo() {
+  return <h1> 🏖️ Far Away 💼 </h1>;
+}
+function Form() {
+  return (
+    <form className="add-form" action="">
+      <h3>What you need your trip ?👽 </h3>
+      <select name="" id="">
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num}>{num}</option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item, index) => (
+          <Item key={index} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Status() {
+  return (
+    <footer className="stats">
+      <em>You have X in you inventory, and you already packed X.🎒</em>
+    </footer>
+  );
 }
 
 export default App;
